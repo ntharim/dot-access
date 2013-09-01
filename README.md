d055b8ec11b0aff01c4e092979d18310aaa1d0d3
Nathan <ntran013@gmail.com>
2013-09-02 02:40:30 +1000

    Added dotAccess function

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-dot-access
-==========
+
+# dot-access
+
+  Access object properties using dot notation
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install Ntran013/dot-access
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,24 @@
+function dotAccess(obj, path) {
+	var parts = path.split('.');
+	var value = obj;
+	for (var i = 0; i < parts.length; i++) {
+		try {
+			value = value[parts[i]];
+		} catch (e) {
+			value = undefined;
+			break;
+		}
+	}
+
+	return value;
+}
+
+// Test
+// console.log(dotAccess({
+// 	user: {
+// 		name: {
+// 			first: 'nathan',
+// 			last: 'tran'
+// 		}
+// 	}
+// }, 'user.name.last'));
