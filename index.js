603592530a47f4ebf0cf4f5e83b8e0dbba8a93f6
Nathan <ntran013@gmail.com>
2013-09-02 04:14:57 +1000

    changed indent

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 exports.get = function (obj, path) {
-	var parts = path.split('.');
-	var value = obj;
+  var parts = path.split('.');
+  var value = obj;
 
-	for (var i = 0; i < parts.length; i++) {
-		try {
-			value = value[parts[i]];
-		} catch (e) {
-			value = undefined;
-			break;
-		}
-	}
+  for (var i = 0; i < parts.length; i++) {
+    try {
+      value = value[parts[i]];
+    } catch (e) {
+      value = undefined;
+      break;
+    }
+  }
 
-	return value;
+  return value;
 };
 
 exports.set = function (obj, path, value) {
-	var parts = path.split('.');
-	var child = parts.pop();
-	var parent = parts.length === 0 ? obj : exports.get(obj, parts.join('.'));
+  var parts = path.split('.');
+  var child = parts.pop();
+  var parent = parts.length === 0 ? obj : exports.get(obj, parts.join('.'));
 
-	if (parent && typeof parent === 'object') {
-		parent[child] = value;
-	}
+  if (parent && typeof parent === 'object') {
+    parent[child] = value;
+  }
 };
